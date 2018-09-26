7fc3fb154120976dff15e41f56fd1c9345906c33
josue rojas <josuerojas.rojas@gmail.com>
2018-09-26 17:48:44 -0400

    nav now sticks when active in mobile; and other nav improvements

--- _includes/partial_assets/js/navbar.js
+++ _includes/partial_assets/js/navbar.js
@@ -1,12 +1,18 @@
 $(document).ready(()=>{
-  $('.nav-bar').click(function(){
-    $nav = $(this);
+  $('.nav-bar .hamburger-menu').click(function(){
+    $nav = $(this).closest('.nav-bar');
     $nav.toggleClass(function(index, name, state){
       // TODO: menu height should be outside calc and should be only calc in initial and resize
-      const menuHeight = name === 'nav-bar active' ? 0 :  $( window ).outerHeight() - $nav.outerHeight();
-      const bottom  = name === 'nav-bar active' ? 0 : -menuHeight
-      $nav.find('.nav-list-wrapper').css({'height': menuHeight, 'bottom': bottom});
+      const menuHeigt = name === 'nav-bar active' ? 0 : `calc(100vh - ${$nav.outerHeight()}px)`;
+      const fixedContent = name === 'nav-bar active' ? 'initial' : 'fixed';
+      $nav.find('.nav-list-wrapper').css({'height': menuHeigt});
+      // NOTE: content must be in a wrapper to stop it from moving when navbar is active
+      $('.content-wrapper').toggleClass('active-nav');
       return 'active';
     });
+  });
+
+  $( window ).resize(function() {
+    $('nav.active .nav-list-wrapper').css({'height': `calc(100vh - ${$('.nav-bar').outerHeight()}px)`})
   });
 });
